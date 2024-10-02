c7b39e04b3f7c8901ee7bf69c6d1a453b9c3137f
purushottamdeshmukh <deshmukh259.2010@gmail.com>
2024-10-02 17:34:33 +0530

    init

---
+++ src/main/java/com/elk_stack/elk_stack/ElkStackApplication.java
@@ -0,0 +1,13 @@
+package com.elk_stack.elk_stack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElkStackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElkStackApplication.class, args);
+	}
+
+}
